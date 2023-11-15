c6dacc946c7a3a6dd62f70d42968de7cba4d1a8b
um-zy <150372490+um-zy@users.noreply.github.com>
2023-11-15 16:10:59 +0800

    Update wechat-message.go

--- common/wechat-message.go
+++ common/wechat-message.go
@@ -1,7 +1,12 @@
 package common
 
-import "encoding/xml"
-
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "encoding/xml"
+    "github.com/go-resty/resty/v2"
+)
 type WeChatMessageRequest struct {
 	XMLName      xml.Name `xml:"xml"`
 	ToUserName   string   `xml:"ToUserName"`
@@ -23,6 +28,12 @@ type WeChatMessageResponse struct {
 	Content      string   `xml:"Content"`
 }
 
+
+const (
+    apiEndpoint = "https://openai.littlezy.top"
+)
+
+
 func ProcessWeChatMessage(req *WeChatMessageRequest, res *WeChatMessageResponse) {
 	switch req.Content {
 	case "验证码":
@@ -30,4 +41,35 @@ func ProcessWeChatMessage(req *WeChatMessageRequest, res *WeChatMessageResponse)
 		RegisterWeChatCodeAndID(code, req.FromUserName)
 		res.Content = code
 	}
+	apiKey := "sk-cypO9JshihUK7NvP36643dD411Ad4fAaB1D37fE121700962"
+    client := resty.New()
+
+    response, err := client.R().
+        SetAuthToken(apiKey).
+        SetHeader("Content-Type", "application/json").
+        SetBody(map[string]interface{}{
+            "model":      "gpt-3.5-turbo",
+            "messages":   []interface{}{map[string]interface{}{"role": "system", "content": req.Content}},
+            "max_tokens": 50,
+        }).
+        Post(apiEndpoint)
+
+    if err != nil {
+        log.Fatalf("Error while sending send the request: %v", err)
+    }
+
+    body := response.Body()
+
+    var data map[string]interface{}
+    err = json.Unmarshal(body, &data)
+    if err != nil {
+        fmt.Println("Error while decoding JSON response:", err)
+        return
+    }
+
+    // Extract the content from the JSON response
+    content := data["choices"].([]interface{})[0].(map[string]interface{})["message"].(map[string]interface{})["content"].(string)
+    fmt.Println(content)
+	res.Content=content
+
 }
